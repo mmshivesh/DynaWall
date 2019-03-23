5efe82915f44c84e3fb59c969ea8f1a9885c3115
Shivesh M M <mmshivesh@gmail.com>
2019-03-23 16:14:35 +0530

    Initial Commit

---
+++ DynaWall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DynaWall
+//
+//  Created by Shivesh M M on 23/03/19.
+//  Copyright © 2019 hyperionStudios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DynaWall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DynaWall
+//
+//  Created by Shivesh M M on 23/03/19.
+//  Copyright © 2019 hyperionStudios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
